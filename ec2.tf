511e1603802941bef4e9101d1ea747cb63ab82dd
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-08 01:31:29 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+#creating ec2 instance ec2.tf
+resource "aws_instance""demoinstance2" {
+ami="ami-0261755bbcb8c4a84"
+instance_type="t2.micro"
+key_name="ajay"
+vpc_security_group_ids=["${aws_security_group.demosg.id}"]
+subnet_id=aws_subnet.public-subnet-1.id
+associate_public_ip_address=true
+user_data=file("data.sh")
+tags={
+Name="my public insatance2"
+}
