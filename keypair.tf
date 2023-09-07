584e1510d97949346700599abb03d57ec8fe283f
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-08 01:30:43 +0530

    Create keypair.tf

---
+++ keypair.tf
@@ -0,0 +1,13 @@
+#creating key pair key.tf
+resource "aws_key_pair" "ajay" {
+key_name = "ajay"
+public_key = tls_private_key.rsa.public_key_openssh
+}
+resource "tls_private_key" "rsa" {
+algorithm = "RSA"
+rsa_bits =4096
+}
+resource "local_file" "tf-key" {
+content = tls_private_key.rsa.private_key_pem
+filename = "ajay"
+}
