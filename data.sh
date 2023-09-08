29e0586c90472b5ed81d42e3ae73be688fc8daaa
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-08 16:34:59 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,8 +1,8 @@
 #creating user data data.sh
 #! /bin/bash
 sudo yum install -y git 
-sudo git clone https://github.com/GOUSERABBANI44/fish.git
-cd fish
+sudo git clone https://github.com/GOUSERABBANI44/Portfolio.git
+cd Portfolio
 sudo yum install -y python3-pip
 pip3 install -r requirements.txt
 python3 app.py
