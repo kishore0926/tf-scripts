b410e59c29cf459b0c5d5c3c3d3affed6ebba41c
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-08 11:35:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 #creating ec2 instance ec2.tf
-resource "aws_instance""instance1" {
-ami="ami-0db1894e055420bc0"
+resource "aws_instance""instance1"{
+ami="ami-0e047ce9149262f82"
 instance_type="t2.micro"
 key_name="ajay"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
