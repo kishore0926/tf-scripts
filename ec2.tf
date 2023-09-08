09e071c47376c90ac763b42fdc7539046e977d5c
ajay77777777 <141303594+ajay77777777@users.noreply.github.com>
2023-09-08 10:27:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 #creating ec2 instance ec2.tf
 resource "aws_instance""demoinstance2" {
-ami="ami-0261755bbcb8c4a84"
+ami="ami-0e047ce9149262f82"
 instance_type="t2.micro"
 key_name="ajay"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
@@ -8,5 +8,5 @@ subnet_id=aws_subnet.public-subnet-1.id
 associate_public_ip_address=true
 user_data=file("data.sh")
 tags={
-Name="my public insatance2"
+Name="my public insatance1"
 }
