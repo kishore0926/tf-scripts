b63fd7e663e58ca9a61f350dafa0c1b1689094df
Srikanth011199 <140722653+Srikanth011199@users.noreply.github.com>
2023-09-27 05:29:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 #creating ec2 instance ec2.tf
 resource "aws_instance""instance1"{
 ami="ami-0f409bae3775dc8e5"
-instance_type="t2.micro"
+instance_type="t2.small"
 key_name="srikanth"
 vpc_security_group_ids=["${aws_security_group.demosg.id}"]
 subnet_id=aws_subnet.public-subnet-1.id
